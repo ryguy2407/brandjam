8d4e03ba3f3b425c01e467b83640df75d35e049c
Ryan Murray <ryanmurrayemail@gmail.com>
2020-08-21 15:56:27 +1000

    updated breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
     fontFamily: {
       'body': ['"Work Sans"', 'Arial', 'sans-serif'],
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'xxl': '1700px',
+      // => @media (min-width: 1700px) { ... }
+    },
     fontSize: {
       'xs': '.75rem',
       'sm': '.875rem',
